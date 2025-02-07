a48381f96358f654a3c5410115798c1cee573be7
Joshua Simanullang <joshua.simanullang@student.manchester.ac.uk>
2025-02-07 23:10:36 +0000

    Create script for loading into postgresql

---
+++ etl_script.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# db connection parameters
+DB_NAME = "toxigen_db"
+DB_USER = "postgres"
+DB_PASSWORD = "Supahotfire21!"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+# create db conn
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+df = pd.read_csv("../data/toxigen_cleaned.csv")
+
+df.to_sql("toxigen_data", engine, if_exists="append", index=False)
+
+print("Data successfully loaded into PostgreSQL!")
