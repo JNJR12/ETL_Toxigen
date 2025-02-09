bbf63978b037a444d12f766c93317986dc203751
Joshua Simanullang <joshua.simanullang@student.manchester.ac.uk>
2025-02-09 13:25:58 +0000

    Benchmarking for optimisations

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,22 +1,33 @@
 import pandas as pd
 import re
 from sqlalchemy import create_engine
+import time
+import psutil
 
 # db conn parameters
 DB_NAME = "toxigen_db"
 DB_USER = "postgres"  
-DB_PASSWORD = "password"  
+DB_PASSWORD = "Supahotfire21!"  
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
+start_db = time.time()
 # postgresql connection
 engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+end_db = time.time()
 
+start_time = time.time()
 # extract data
+start_extract = time.time()
 file_path = "../data/toxigen.csv"
 df = pd.read_csv(file_path)
+end_extract = time.time()
+print(f"Extraction Time: {end_extract - start_extract:.2f} seconds")
+
+
 
 # transform data
+start_transform = time.time()
 def clean_prompt(text):
     if isinstance(text, str):
         text = text.strip()  # trim space
@@ -53,12 +64,23 @@ df["prompt_label"] = df["prompt_label"].map({0: "Benign", 1: "Toxic"})
 
 df = df.drop_duplicates()
 
+# improve memory usage
 df["generation_method"] = df["generation_method"].astype("category")
 df["group"] = df["group"].astype("category")
 df["prompt_label"] = df["prompt_label"].astype("category")
 df["toxicity_level"] = df["toxicity_level"].astype("category")
 
+end_transform = time.time()
+print(f"Transformation Time: {end_transform - start_transform:.2f} seconds")
+
 # load data into PostgreSQL
+start_load = time.time()
 df.to_sql("toxigen_data", engine, if_exists="replace", index=False)
+end_load = time.time()
+print(f"Load Time: {end_load - start_load:.2f} seconds")
 
 print("ETL Pipeline completed successfully! Data loaded into PostgreSQL.")
+end_time = time.time()
+print(f"Total ETL runtime: {end_time - start_time:.2f} seconds")
+
+print(f"Memory Usage: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
